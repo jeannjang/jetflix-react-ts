6152c1f100aa74e81467576683842a147401da4d
Jean <jeanjyjh@gmail.com>
2024-12-16 11:29:04 +1300

    feat: Add Series component with trending series banner
    - Implement Series component using react-query to fetch trending tv-series
    - Display a Banner component with the first trending tv-series

--- src/routes/Series.tsx
+++ src/routes/Series.tsx
@@ -1,5 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { getTrendingTvSeries, IGetTvSeriesResponse } from "../api/tvApi";
+import styled from "styled-components";
+import Banner from "../components/common/Banner";
+
+const Wrapper = styled.div`
+  height: 200vh;
+`;
+
+const Loading = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => props.theme.white.primary};
+`;
+
 function Series() {
-  return <></>;
+  const { data, isLoading } = useQuery<IGetTvSeriesResponse>({
+    queryKey: ["movies", "trending"],
+    queryFn: getTrendingTvSeries,
+  });
+
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        data?.results[0] && <Banner mediaData={data.results[0]} />
+      )}
+    </Wrapper>
+  );
 }
 
 export default Series;
