f14bec31e34e38d7b50912ab20754f3a2412ee92
Jean <jeanjyjh@gmail.com>
2024-12-16 11:27:59 +1300

    feat: Add Movies component with trending movies banner
    - Implement Movies component using react-query to fetch trending movies
    - Display a Banner component with the first trending movie

--- src/routes/Movies.tsx
+++ src/routes/Movies.tsx
@@ -1,5 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { getTrendingMovies, IGetMoviesResponse } from "../api/moviesApi";
+import styled from "styled-components";
+import Banner from "../components/common/Banner";
+
+const Wrapper = styled.div`
+  height: 200vh;
+`;
+
+const Loading = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => props.theme.white.primary};
+`;
+
 function Movies() {
-  return <></>;
+  const { data, isLoading } = useQuery<IGetMoviesResponse>({
+    queryKey: ["movies", "trending"],
+    queryFn: getTrendingMovies,
+  });
+
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        data?.results[0] && <Banner mediaData={data.results[0]} />
+      )}
+    </Wrapper>
+  );
 }
 
 export default Movies;
