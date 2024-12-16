5a11fd0b8807396f9c38081633ab277c112710e6
Jean <jeanjyjh@gmail.com>
2024-12-17 12:40:01 +1300

    chore: set API cache duration to 24 hours

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,14 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import React from "react";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 24 * 60 * 60 * 1000, // 24 hours
+      gcTime: 24 * 60 * 60 * 1000,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
